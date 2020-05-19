ca70c7fb6432f2aff023f0e0b1549d1fce38ec66
Abdullah Fahmi <abdullahfahmi1997@gmail.com>
2020-05-19 15:37:57 +0700

    add CORS

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
 $router->get('/', function () use ($router) {
     return view('home');
 });
